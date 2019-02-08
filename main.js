4f7be468d950a4e0c519b2a1380a40c1d8e1e86f
Bodia Valeur <bodiavaleur@gmail.com>
2019-02-08 19:22:55 +0200

    Shototam

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 document.getElementById('slider-left').onclick = sliderLeft;
 var left = 0;
 var polosa = document.getElementById('polosa');
+const images = document.querySelectorAll('img');
+const info = document.getElementById('info');
+const btnInfo = document.getElementById('info-close');
 
 function sliderLeft() {
-
-    left = left - 128;
-    if (left < -512) {
-        left = 0;
-
-    }
-    polosa.style.left = left + 'px';
+  left = left - 128;
+  if (left < -512) {
+    left = 0;
+  }
+  polosa.style.left = left + 'px';
 }
+
+btnInfo.addEventListener('click', () => {
+  info.style.display = 'none';
+});
+
+images.forEach(img => {
+  img.addEventListener('click', () => {
+    info.style.display = 'flex';
+    info.children[0].src = img.src;
+  });
+});
