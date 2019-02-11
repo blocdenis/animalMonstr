be3b3a90370d69a9cf378889ec992eda8222d27e
blocdenis <46189072+blocdenis@users.noreply.github.com>
2019-02-11 22:28:54 +0100

    Add files via upload
    added quiz

---
+++ quiz_answer.js
@@ -0,0 +1,30 @@
+let answers = {
+    "b": {
+        "description": "Вы —зайчик ",
+        "image": "bunny.png",
+    },
+    "c": {
+        "description": "Вы — корова",
+        "image": "cow.png",
+    },
+    "d": {
+        "description": "Вы — Пес",
+        "image": "dog.png",
+    },
+    "duck": {
+        "description": "Вы — утка",
+        "image": "duck.png",
+    },
+    "g": {
+        "description": "Вы — жираф.",
+        "image": "giraf.png",
+    },
+    "m": {
+        "description": "Вы — мартышка",
+        "image": "monkey.png",
+    },
+    "mouse": {
+        "description": "Вы - мышка",
+        "image": "mouse.png"
+    }
+};
---
+++ quiz_data.js
@@ -0,0 +1,121 @@
+const quiz = [{
+        "q": "sd12",
+        "a": {
+            "b": "asd",
+            "c": "asfas",
+            "d": "fds",
+            "duck": "ad?",
+            "g": "efasd",
+            "m": "sd",
+            "mouse": "asd",
+        }
+    },
+    {
+        "q": "asd",
+        "a": {
+            "b": "asd",
+            "c": "asfas",
+            "d": "fds",
+            "duck": "ad?",
+            "g": "efasd",
+            "m": "sd",
+            "mouse": "asd",
+        }
+    },
+    {
+        "q": "asds",
+        "a": {
+            "b": "asd",
+            "c": "asfas",
+            "d": "fds",
+            "duck": "ad?",
+            "g": "efasd",
+            "m": "sd",
+            "mouse": "asd",
+        }
+    },
+    {
+        "q": "asdas",
+        "a": {
+            "b": "asd",
+            "c": "asfas",
+            "d": "fds",
+            "duck": "ad?",
+            "g": "efasd",
+            "m": "sd",
+            "mouse": "asd",
+        }
+    },
+    {
+        "q": "wfrfe",
+        "a": {
+            "b": "asd",
+            "c": "asfas",
+            "d": "fds",
+            "duck": "ad?",
+            "g": "efasd",
+            "m": "sd",
+            "mouse": "asd",
+        }
+    },
+    {
+        "q": "ascx",
+        "a": {
+            "b": "asd",
+            "c": "asfas",
+            "d": "fds",
+            "duck": "ad?",
+            "g": "efasd",
+            "m": "sd",
+            "mouse": "asd",
+        }
+    },
+    {
+        "q": "qwefsad",
+        "a": {
+            "b": "asd",
+            "c": "asfas",
+            "d": "fds",
+            "duck": "ad?",
+            "g": "efasd",
+            "m": "sd",
+            "mouse": "asd",
+        }
+    },
+    {
+        "q": "asvcc",
+        "a": {
+            "b": "asd",
+            "c": "asfas",
+            "d": "fds",
+            "duck": "ad?",
+            "g": "efasd",
+            "m": "sd",
+            "mouse": "asd",
+        }
+    },
+    {
+        "q": "asdasd",
+        "a": {
+            "b": "asd",
+            "c": "asfas",
+            "d": "fds",
+            "duck": "ad?",
+            "g": "efasd",
+            "m": "sd",
+            "mouse": "asd",
+        }
+    },
+    {
+        "q": "asac",
+        "a": {
+            "b": "asd",
+            "c": "asfas",
+            "d": "fds",
+            "duck": "ad?",
+            "g": "efasd",
+            "m": "sd",
+            "mouse": "asd",
+        }
+    },
+];
